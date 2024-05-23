e122544f934211cd4d791a6101147e5b6f9711c3
Mutia Pegi Intanswari <149048644+Mutiaacode@users.noreply.github.com>
2024-05-23 16:19:03 +0700

    Update Hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -73,7 +73,7 @@ function Hero() {
           <HeroRight>
             <ScrollAnimation animateIn="fadeIn">
               <Image
-                src="/ryo.png"
+                src="/m.png"
                 alt="mutia"
               />
             </ScrollAnimation>
