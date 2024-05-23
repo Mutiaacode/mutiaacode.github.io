06b240a8fad2da45d5f5a19d4d587c3cf53ac8cb
Mutia Pegi Intanswari <149048644+Mutiaacode@users.noreply.github.com>
2024-05-23 11:55:12 +0700

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -20,7 +20,7 @@ function Projects() {
       </svg>
       <div className="ProjectWrapper" id="projects">
         <div className="Container">
-          <div className="SectionTitle">Project Terbaru</div>
+          <div className="SectionTitle">Projects</div>
           <ProjectCard />
         </div>
       </div>
